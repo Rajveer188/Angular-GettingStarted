2808fa5c6eb18188a9e70b6269c9585d3e809ff2
Rajveer188 <veerkajle188@gmail.com>
2025-03-06 15:02:11 +0530

    [Rajveer] Add. validate user name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,10 +15,20 @@ export class AppComponent {
   imageURL = "../assets/BL_logo_square.png"
   webURL = "https://www.BridgeLabz.com"
   userName = ""
+  userNameError = ""
 
   openWebsite($event: any){
     console.log("logo is clicked", $event);
     
     window.open(this.webURL, "_blank")
+  }
+  validateUsername($event: any){
+    console.log("change event occured ", $event.data);
+    const regex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$')
+    if(regex.test(this.userName)){
+      this.userNameError = ""
+      return;
+    }
+    this.userNameError = "Name is not valid";
   }
 }
