0df35f9bf88f641baa71d9d60ef7e03bd69a5cda
Rajveer188 <veerkajle188@gmail.com>
2025-03-06 14:47:02 +0530

    [Rajveer] Add. display Hello from BridgeLabz with a user inputted name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { log } from 'node:console';
+import { FormsModule } from '@angular/forms';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   message = 'Hellow from BridgeLabz';
   imageURL = "../assets/BL_logo_square.png"
   webURL = "https://www.BridgeLabz.com"
+  userName = ""
 
   openWebsite($event: any){
     console.log("logo is clicked", $event);
